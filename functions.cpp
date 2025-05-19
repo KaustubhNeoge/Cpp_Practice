239a9c4a00e831990e5dc8a6f70e9d6321486330
Kaustubh <136223697+KaustubhNeoge@users.noreply.github.com>
2025-05-20 01:57:52 +0530

    Add files via upload

---
+++ functions.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+void showMenu() {
+	cout << "1> Start." << endl;
+	cout << "2> Accelerate." << endl;
+	cout << "3> Decelerate." << endl;
+	cout << "4> Stop." << endl;
+}
+
+int main() {
+	showMenu();
+	cout << "Enter an option: " << flush;
+	int opt;
+	cin >> opt;
+	switch (opt) {
+	case 1:
+		cout << "Staring the engine..." << endl;
+		break;
+	case 2:
+		cout << "Accelerating by 1 unit." << endl;
+		break;
+	case 3:
+		cout << "Decelerating by 1 unit." << endl;
+		break;
+	case 4:
+		cout << "Stopping the engine...";
+		break;
+	default:
+		cout << "Enter an appropriate option.";
+	}
+}
